cc266848b1b393b32bfb6a379a9d2430c9240da5
Unknown <yann.ravinet@gmail.com>
2018-08-04 07:18:19 +0200

    add edit post

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -52,5 +54,28 @@ class PostsController extends Controller
         }
 
         return $this->render('posts/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/posts/{id}/edit", name="post_edit_path", requirements={"id": "[0-9]+"})
+     * @Method({"GET", "PATCH"})
+     */
+    public function edit(Post $post, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+
+            return $this->redirectToRoute('post_path', ['id' => $post->getId()]);
+        }
+
+        return $this->render('posts/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
     }
 }
